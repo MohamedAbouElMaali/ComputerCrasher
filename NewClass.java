fc02ff3069a54e11510e55061b5198f4568cbde8
MohamedAbouElMaali <96319889+MohamedAbouElMaali@users.noreply.github.com>
2022-09-16 09:26:00 -0400

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+// Java program to illustrate
+// open cmd prompt
+
+class NewClass
+{
+	public static void main(String[] args) throws IOException
+	{
+		while(true) {
+			Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"java C:\\Users\\5924008096\\eclipse-workspace\\Random\\src\\NewClass.java\"");
+		}
+	}
+}
